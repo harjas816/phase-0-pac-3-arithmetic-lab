fce6bd6a059fc47fba9ef484874b8d4e5fcc277b
Harjas Bedi <harjas816@gmail.com>
2023-05-04 11:26:36 -0400

    Completed Lab

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b){
+    return a + b
+}
+
+function subtract(a,b){
+    return a - b
+}
+
+function multiply (a,b){
+    return a * b
+}
+
+function divide (a,b){
+    return a / b
+}
+
+function increment(n){
+    let p = ++n
+    return p
+}
+
+function decrement (n){
+    let p = --n
+    return p
+}
+
+function makeInt(a){
+    return parseInt(a, 10)
+}
+
+function preserveDecimal(a){
+    return parseFloat(a)
+}
